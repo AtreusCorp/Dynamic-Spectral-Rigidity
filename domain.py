8c4bebb808895833d74e123a0c2c822ff28b9b93
Jacopo De Simoi <jacopods@gmail.com>
2019-06-17 23:53:08 -0400

    Cleanup whitespace

--- domain.py
+++ domain.py
@@ -12,8 +12,8 @@ class Domain:
         self.orbits = {}
 
     def import_fourier(self, path):
-        """ Parses a textfile located at path containing fourier coefficients 
-            for the domain. Coefficients are expected one per line. 
+        """ Parses a textfile located at path containing fourier coefficients
+            for the domain. Coefficients are expected one per line.
         """
 
         file = open(path, "r")
@@ -24,36 +24,36 @@ class Domain:
 
     def radius(self, theta):
         """ Computes the radius function at a given angle. Assumed that
-            import_fourier has been called. 
+            import_fourier has been called.
         """
 
         return fourierval((self.fourier, [0]), [0, 1], theta)
 
     def radius_derivative(self, theta):
-        """ Computes the derivative of the radius function at a given angle. 
-            Assumed that import_fourier has been called. 
+        """ Computes the derivative of the radius function at a given angle.
+            Assumed that import_fourier has been called.
         """
 
-        series_terms = [fprod([-n, fmul(2, pi), self.fourier[n]]) 
+        series_terms = [fprod([-n, fmul(2, pi), self.fourier[n]])
                         for n in range(0, len(self.fourier))]
         return fourierval(([0], series_terms), [0, 1], theta)
 
     def radius_second_derivative(self, theta):
-        """ Computes the second derivative of the radius function at a given 
-            angle. Assumed that import_fourier has been called. 
+        """ Computes the second derivative of the radius function at a given
+            angle. Assumed that import_fourier has been called.
         """
 
-        series_terms = [fprod([-1, power(fprod([2, pi, n]), 2), 
-                               self.fourier[n]]) 
+        series_terms = [fprod([-1, power(fprod([2, pi, n]), 2),
+                               self.fourier[n]])
                         for n in range(0, len(self.fourier))]
         return fourierval((series_terms, [0]), [0, 1], theta)
 
     def polar(self, theta):
-        """ Provides an interface for polar coordinates. 
+        """ Provides an interface for polar coordinates.
             Assumed that import_fourier has been called.
         """
         theta_rescaled = fprod([theta, 2, pi])
-        return (fmul(self.radius(theta), cos(theta_rescaled)),  
+        return (fmul(self.radius(theta), cos(theta_rescaled)),
                 fmul(self.radius(theta), sin(theta_rescaled)))
 
     def polar_gradient(self, theta):
@@ -63,14 +63,14 @@ class Domain:
         r = self.radius(theta)
         r_prime = self.radius_derivative(theta)
         theta_rescaled = fprod([theta, 2, pi])
-        grad_x = fsub(fmul(r_prime, cos(theta_rescaled)), 
+        grad_x = fsub(fmul(r_prime, cos(theta_rescaled)),
                       fprod([r, 2, pi, sin(theta_rescaled)]))
-        grad_y = fadd(fmul(r_prime, sin(theta_rescaled)), 
+        grad_y = fadd(fmul(r_prime, sin(theta_rescaled)),
                       fprod([r, 2, pi, cos(theta_rescaled)]))
         return (grad_x, grad_y)
 
     def polar_gradient_norm_deriv(self, theta):
-        """ Returns the derivative of the norm of polar_gradient evaluated at 
+        """ Returns the derivative of the norm of polar_gradient evaluated at
             the point theta.
         """
 
@@ -81,29 +81,29 @@ class Domain:
         radius_second_deriv = self.radius_derivative(theta)
         theta_rescaled = fprod([theta, 2, pi])
         two_pi = fmul(2, pi)
-        
+
         coeff_1 = fsub(radius_second_deriv, fmul(power(two_pi, 2), radius))
         coeff_2 = fprod([2, two_pi, radius_deriv])
 
-        grad_x_prime = fsub(fmul(coeff_1, cos(theta_rescaled)), 
+        grad_x_prime = fsub(fmul(coeff_1, cos(theta_rescaled)),
                             fmul(coeff_2, sin(theta_rescaled)))
-        grad_y_prime = fadd(fmul(coeff_2, cos(theta_rescaled)), 
+        grad_y_prime = fadd(fmul(coeff_2, cos(theta_rescaled)),
                             fmul(coeff_1, sin(theta_rescaled)))
-        norm_deriv = fdiv(fadd(fmul(gradient[0], grad_x_prime), 
-                               fmul(gradient[1], grad_y_prime)), 
+        norm_deriv = fdiv(fadd(fmul(gradient[0], grad_x_prime),
+                               fmul(gradient[1], grad_y_prime)),
                           gradient_norm)
         return norm_deriv
 
 
 def arc_length_coords(domain, x):
-    """ Returns the transformation taking x (in [0, 1]) to the 
+    """ Returns the transformation taking x (in [0, 1]) to the
         corresponding point in the parameterization by arc length.
     """
 
     return quad(lambda t: norm(domain.polar_gradient(t)), [0, x])
 
 def inv_arc_length_coords(domain, s):
-    """ Returns the inverse of the transformation taking x (in [0, 2 pi]) to the 
+    """ Returns the inverse of the transformation taking x (in [0, 2 pi]) to the
         corresponding point in the parameterization by arc length.
     """
 
--- lazutkin_coordinates.py
+++ lazutkin_coordinates.py
@@ -5,7 +5,7 @@ def C(domain):
     """ Returns the C coefficient for the Lazutkin paramterization.
     """
 
-    integrand = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)), 
+    integrand = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)),
                                norm(domain.polar_gradient(t)))
     return power(quad(integrand, [0, 1]), -1)
 
@@ -15,7 +15,7 @@ def lazutkin_param_non_arc(domain, s):
         Generally s is assumed to be the parameterization by arc length.
     """
 
-    integrand = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)), 
+    integrand = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)),
                                norm(domain.polar_gradient(t)))
     return fmul(C(domain), quad(integrand, [0, s]))
 
@@ -24,7 +24,7 @@ def lazutkin_param_non_arc_deriv(domain, s):
         Generally s is assumed to be the parameterization by arc length.
     """
 
-    fnc = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)), 
+    fnc = lambda t: fmul(power(domain.radius(t), fdiv(-2, 3)),
                          norm(domain.polar_gradient(t)))
     return fmul(C(domain), fnc(s))
 
@@ -35,16 +35,16 @@ def lazutkin_param_non_arc_second_deriv(domain, s):
     c = C(domain)
     radius = domain.radius(s)
 
-    fnc = lambda t: fadd(fprod([fdiv(-2, 3), 
-                                power(radius, fdiv(-5, 3)), 
-                                domain.radius_derivative(t), 
+    fnc = lambda t: fadd(fprod([fdiv(-2, 3),
+                                power(radius, fdiv(-5, 3)),
+                                domain.radius_derivative(t),
                                 norm(domain.polar_gradient(t))]),
-                         fmul(domain.polar_gradient_norm_deriv(t), 
+                         fmul(domain.polar_gradient_norm_deriv(t),
                               power(radius, fdiv(-2, 3))))
     return fmul(C(domain), fnc(s))
 
 def lazutkin_weight(domain, x):
     """ Returns the Lazutkin weight of domain at the point x.
     """
-    
+
     return power(fprod([2, C(domain), power(domain.radius(x), fdiv(1, 3))]), -1)
