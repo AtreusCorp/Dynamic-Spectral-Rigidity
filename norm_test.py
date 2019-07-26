7fbb4b06c629535d5dc4f483ec6fdb081169edff
Keirn Yves Ross Munro <keirn.munro@mail.utoronto.ca>
2019-07-25 23:37:21 -0400

    Typos and new api update

--- norm_test.py
+++ norm_test.py
@@ -2,8 +2,8 @@ from operator_norms import *
 
 mp.dps = int(input("Enter mp.dps: "))
 domain = Domain()
-fourer_path = input("Enter a path to a Fourier coefficient file: ")
-domain.import_fourier(fourer_path)
+fourier_path = input("Enter a path to a Fourier coefficient file: ")
+domain.import_fourier_from_file(fourier_path)
 max_j = int(input("Enter max j: "))
 max_q = int(input("Enter max q: "))
 gamma = float(input("Enter gamma: "))
